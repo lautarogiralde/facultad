c3c165177c25171336c5e66b229c5713cca99c41
Lautaro <giraldelautaro2@gmail.com>
2026-05-27 15:37:24 -0300

    practica4

--- cac/practica/practica4/ejercicio4.s
+++ cac/practica/practica4/ejercicio4.s
@@ -22,7 +22,9 @@ fin:    halt
 ;   Con los datos proporcionados va a terminar con r10 en 1
 ;b)	Re-Ejecutar el programa con la opción Configure/Enable Branch Target Buffer habilitada. 
 ;   Explicar la ventaja de usar este método y cómo trabaja.
-;
+;   Con Branch Target Buffer el codigo tiene menos atascos Branch Taken, debido a que cambia la prediccion de salto en caso de equivocarse
+;   Para cada instruccion se salto hay un registro en una tabla donde indica si la vez anterior se salto o no, permite que ejecute la proxima
+;   instruccion y en caso de equivocarse va a perder un ciclo re reloj para hacer el Fetch de la instruccion correcta.
 
 ;c)	Confeccionar una tabla que compare número de ciclos, CPI, RAWs y Branch Taken Stalls para los dos casos anteriores.
-
+;   
